09fa78df4a654d46a3f49a2bcf3daa129afc9625
juhynl <ljuh0928@gmail.com>
2024-04-18 16:41:34 +0900

    feat: add semantics for addition and subtraction

--- Lab3/FMinus/src/FMinus.fs
+++ Lab3/FMinus/src/FMinus.fs
@@ -8,7 +8,15 @@ let rec evalExp (exp: Exp) (env: Env) : Val =
   match exp with
   | Num i -> Int i
   | True -> Bool true
-  | False -> Bool false 
+  | False -> Bool false
+  | Add(e1, e2) -> 
+    match (evalExp e1 env, evalExp e2 env) with
+    | (Int i1, Int i2) -> Int (i1 + i2)
+    | _ -> raise UndefinedSemantics
+  | Sub(e1, e2) -> 
+    match (evalExp e1 env, evalExp e2 env) with
+    | (Int i1, Int i2) -> Int (i1 - i2)
+    | _ -> raise UndefinedSemantics
   | _ -> raise // TODO: fill in the remaining cases.
 
 // Note: You may define more functions.
