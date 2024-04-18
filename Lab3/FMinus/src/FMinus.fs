1f8eb4c29e007b5e4abdc34dc7b808249ebc1b86
juhynl <ljuh0928@gmail.com>
2024-04-18 10:32:38 +0900

    feat: add skeleton codes for F- interpreter

---
+++ Lab3/FMinus/src/FMinus.fs
@@ -0,0 +1,16 @@
+module FMinus
+
+open AST
+open Types
+
+// Evaluate expression into a value, under the given environment.
+let rec evalExp (exp: Exp) (env: Env) : Val =
+  match exp with
+  | Num i -> Int i
+  | _ -> Int (-1) // TODO: fill in the remaining cases.
+
+// Note: You may define more functions.
+
+// The program starts execution with an empty environment. Do not fix this code.
+let run (prog: Program) : Val =
+  evalExp prog Map.empty
